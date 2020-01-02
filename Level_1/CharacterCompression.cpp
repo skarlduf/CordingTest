5dbd3fc81f485165c62f9203fdb205b6bb7f369c
Giyeol Nam <skarlduf11@gmail.com>
2020-01-02 18:44:09 +0900

    cording Test

---
+++ Level_1/CharacterCompression.cpp
@@ -0,0 +1,78 @@
+﻿
+
+
+
+
+
+
+#include "pch.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+
+using namespace std;
+
+
+
+int solution(string s) {
+
+	int answer = s.length();
+	int len = s.length();
+
+	for (int i = 1; i <= s.length() / 2; ++i) {
+		int pos = 0;
+		int len = s.length();
+
+		for (;;) {
+			string unit = s.substr(pos, i);
+			pos += i;
+
+			if (pos >= s.length()) break;
+
+			int cnt = 0;
+			for (;;) {
+				if (unit.compare(s.substr(pos, i)) == 0) {
+
+
+					++cnt;
+					pos += i;
+				}
+				else {
+					break;
+				}
+
+			}
+
+			if (cnt > 0) {
+				len -= i * cnt;
+
+				if (cnt < 9) len += 1;
+				else if (cnt < 99) len += 2;
+				else if (cnt < 999) len += 3;
+				else len += 4;
+
+
+
+
+			}
+
+		}
+
+
+		answer = min(answer, len);
+	}
+
+	return answer;
+
+}
+
+
+int main()
+{
+    std::cout << solution("aaaaaabbbbbbb");;
+
+	
+}
+
